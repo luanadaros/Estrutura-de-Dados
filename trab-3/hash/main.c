d7072bbe66fc23008c810fbdc73d58ab78ef6e35
Luana <luana.r.almeida@edu.ufes.br>
2023-12-07 09:03:59 -0300

    testes hash

--- trab-3/hash/main.c
+++ trab-3/hash/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "hash.h"
 
 int hash_function_string(HashTable * h, void * key){
-    int h, a = 31415, b = 27183;
+    int k, a = 31415, b = 27183;
     char * str = key;
     int size = hash_table_size(h);
 
-    for(h = 0; *str != '\0'; str++, a = a*b % (size - 1)){
-        h = (a*h + *str) % size;
+    for(k = 0; *str != '\0'; str++, a = a*b % (size - 1)){
+        k = (a*k + *str) % size;
     }
 
-    return h;
+    return k;
 }
 
 int cmp_key(void * k1, void * k2){
@@ -22,3 +23,18 @@ int cmp_key(void * k1, void * k2){
     return strcmp(c1, c2);
 }
 
+int main(){
+    HashTable * h = hash_table_construct(701, hash_function_string, cmp_key);
+
+    char word[20];
+    int i = 0;
+
+    while(!strcmp(word, "end")){
+        scanf("%s", word);
+        scanf("%*c");
+
+        hash_table_set(h, word, i);
+
+        i++;
+    }
+}
